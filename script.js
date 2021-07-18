ff9741ebbe8c3842c356e031b49a864d3fe60c8c
Nilkeson Souza <nilkesonsouza@gmail.com>
2021-07-17 22:19:28 -0300

    Certo até aqui

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const toggle = document.querySelector('.toggle')
+const sidebar = document.querySelector('.sidebar')
+const titleH3 = document.querySelector('.title-h3')
+const ulSubMenu = document.querySelector('.toggle-profile')
+const ulSubMenuProfile = document.querySelector('.ul-sub-menu-profile')
+const liA = document.querySelector('#li-a')
+
+
+
+ulSubMenu.addEventListener('click', () => {
+    ulSubMenuProfile.classList.toggle('show')
+})
 
 toggle.addEventListener('click', () => {
     toggle.classList.toggle('active')
+    sidebar.classList.toggle('active')
+    titleH3.classList.toggle('active')
+    liA.classList.toggle('active')
 })
