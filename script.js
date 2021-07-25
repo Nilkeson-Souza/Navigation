fb5ef4f4e8297d1b61c76d1642ca1d7ab26268cc
Nilkeson Souza <nilkesonsouza@gmail.com>
2021-07-25 15:32:17 -0300

    Até aqui tudo bem

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ const titleH3 = document.querySelector('.title-h3')
 const ulSubMenu = document.querySelector('.toggle-profile')
 const ulSubMenuProfile = document.querySelector('.ul-sub-menu-profile')
 const liA = document.querySelector('#li-a')
-
+const btnCategoria = document.querySelector('.btn-categoria')
+const categoria = document.querySelector('#modalAdicionar');
+const btnCancelarCategoria = document.querySelector('.btn-cancelar-categoria')
+const btnCancelarCategoriaAlterar = document.querySelector('.btn-cancelar-categoriaAlterar')
+const alterarCategoria = document.querySelector('#modalAlterar');
+const modalRemover = document.querySelector('#modalRemover')
+const btnCancelarCategoriaRemover = document.querySelector('.btn-cancelar-categoriaRemover')
+const modalAlerta = document.querySelector('#modalAlerta')
+const cancelarAlerta = document.querySelector('#cancelarAlerta')
+const modalProgress = document.querySelector('#modalProgress')
 
 
 ulSubMenu.addEventListener('click', () => {
@@ -16,4 +25,60 @@ toggle.addEventListener('click', () => {
     sidebar.classList.toggle('active')
     titleH3.classList.toggle('active')
     liA.classList.toggle('active')
+})
+
+/********MODAL ADD CATEGORIA******** *
+btnCategoria.addEventListener('click', () => {
+    abrirModal(categoria)
+})
+
+btnCancelarCategoria.addEventListener('click', () => {
+    fecharModal(categoria)
+})*/
+
+const abrirModal = (element) => {
+    element.style.display = 'block';
+}
+const abrirModalflex = (element) => {
+    element.style.display = 'flex';
+}
+
+const fecharModal = (element) => {
+    element.style.display = 'none'
+}
+
+/********MODAL ALTERAR CATEGORIA******** *
+btnCategoria.addEventListener('click', () => {
+    abrirModal(alterarCategoria)
+})
+
+btnCancelarCategoria.addEventListener('click', () => {
+    fecharModal(alterarCategoria)
+})*/
+
+/********MODAL REMOVER CATEGORIA******** 
+btnCategoria.addEventListener('click', () => {
+    abrirModal(modalRemover)
+})
+
+btnCancelarCategoriaRemover.addEventListener('click', () => {
+    fecharModal(modalRemover)
+})*/
+
+/********MODAL ALERTA********
+btnCategoria.addEventListener('click', () => {
+    abrirModal(modalAlerta)
+})
+
+cancelarAlerta.addEventListener('click', () => {
+    fecharModal(modalAlerta)
+})
+*/
+
+btnCategoria.addEventListener('click', () => {
+    abrirModalflex(modalProgress)
+})
+
+btnCancelarCategoria.addEventListener('click', () => {
+    fecharModal(modalProgress)
 })
