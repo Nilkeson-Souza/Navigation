0a7cb865032d4e34c51b6b8d12d770f94b595db0
Nilkeson Souza <nilkesonsouza@gmail.com>
2021-07-27 22:36:14 -0300

    ok

--- script/requisicoes.js
+++ script/requisicoes.js
@@ -1,1 +1,59 @@
 const db = firebase.firestore()
+
+/*db.collection("categorias").doc('1').get().then((doc) => {
+    
+    if(doc.exists){
+        
+        const dados = doc.data()
+
+        const nome = dados.nome
+
+        console.log(nome);
+
+    } else {
+        console.log('Não existe');
+    }
+
+});*/
+
+db.collection("categorias").onSnapshot((documentos) => {
+
+    documentos.docChanges().forEach(changes => {
+
+        if (changes.type === 'added') {
+
+            const documento = changes.doc
+
+            const dados = documento.data()
+
+            const key = documento.id
+
+            console.log(`Nome da pasta ${key}`);
+
+        }
+        else if (changes.type === 'modified') {
+
+            const documento = changes.doc
+
+            const dados = documento.data()
+
+            const key = documento.id
+
+            console.log(`Nome da pasta ${key}`);
+
+        }
+        else if (changes.type === 'removed') {
+
+            const documento = changes.doc
+
+            const dados = documento.data()
+
+            const key = documento.id
+
+            console.log(`Nome da pasta ${key}`);
+
+        }
+
+    });
+
+});
